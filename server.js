cd92adb87ff9e19ead0967c6eb084cc301b19e1c
Henri Puusep <henripuusep12@gmail.com>
2023-12-13 16:09:22 +0200

    lõpp

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const todoRouter = require();
+const todoRouter = require('./routes/todo.routes');
 const dotenv = require('dotenv');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 
-app.use('/', );
+app.use('/', todoRouter);
 
-app.listen(, () => {
+app.listen(port, () => {
     console.log('Server is running on Port 3000.');
 });
